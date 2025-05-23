9c6e96b218b0b9e05d3c5868f591c3b35031f7f2
Ahmed-najib SAKHRI <AhmNejSak>
2025-05-23 19:39:30 +0200

    test deployment multi envs

---
+++ tests/test-env.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+# Choisir le fichier .env à charger
+ENV_FILE = ".env.lyon"  # ou ".env.test"
+
+print(f"🔄 Chargement du fichier {ENV_FILE}")
+load_dotenv(dotenv_path=ENV_FILE)
+
+print("🔍 Variables d'environnement chargées :")
+print("INPUT_DIR  =", os.getenv("INPUT_FOLDER_ID"))
+print("OUTPUT_DIR =", os.getenv("OUTPUT_FOLDER_ID"))
+print("ARCHIVE_DIR =", os.getenv("ARCHIVE_FOLDER_ID"))
+
+
+# Test d'écriture dans OUTPUT_DIR
+output_dir = os.getenv("OUTPUT_FODLER_ID")
+if output_dir:
+    try:
+        test_path = os.path.join(output_dir, "test_output.txt")
+        with open(test_path, "w") as f:
+            f.write("✅ Test d'écriture réussi depuis test_env.py")
+        print(f"✍️ Écriture réussie dans : {test_path}")
+    except Exception as e:
+        print(f"❌ Erreur d'écriture dans OUTPUT_DIR : {e}")
+else:
+    print("❌ OUTPUT_FOLDER_ID non défini.")
